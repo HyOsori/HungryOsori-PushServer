4e1b69bc4734cdee8ca5befb74b94a5ba5ec89d1
doomsheart <doomsheart@naver.com>
2016-08-31 02:32:16 +0900

    changed view.py pushing from python requests to execute local python file using subprocess module.

--- push/views.py
+++ push/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 import requests 
 import subprocess
 
-#def push_urls(request):
-subprocess.call(["python /mnt/c/Users/dooms/HungryOsori-PushServer/push/pushing.py"], shell=True)
-subprocess.call(["python /mnt/c/Users/dooms/HungryOsori-PushServer/push/pushingios.py"], shell=True)
+def push_urls(request):
+    currentDir = subprocess.check_output(["pwd"])
+    currentDir = currentDir[:-1] #deleting the '\n' word at the end that printed by defalut
+    subprocess.call(["python "+currentDir+"/push/pushing.py"], shell=True)
+    subprocess.call(["python "+currentDir+"/push/pushingios.py"], shell=True)
 #   print(response.text)
 
 
