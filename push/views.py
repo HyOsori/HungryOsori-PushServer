16fd669ffed184d5d27805f5f17ef1e0d344ea9b
doomsheart <doomsheart@naver.com>
2016-09-19 18:44:14 +0900

    changed to python3

--- push/views.py
+++ push/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
+import json
+import httplib2
+import urllib
 import requests 
 import subprocess
 from django.conf import settings
 from pyfcm import FCMNotification
+from django.views.decorators.csrf import csrf_exempt
 api_list=[]
 push_api_and = settings.PUSH_API_AND
 push_api_ios = settings.PUSH_API_IOS
 api_list.append(push_api_and)
 api_list.append(push_api_ios)
 
-
+@csrf_exempt
+    
 def push_urls(request):
     for i in api_list:
         push_service = FCMNotification(api_key= i)
@@ -18,29 +23,44 @@ def push_urls(request):
         title = u"Test!"
         message = u"this is the test push"
         
-        #sup = "fOqyB9yCq3o:APA91bHH4fQu5Bipea_9QEYsnduX0kQ_bMa7FlcaMeP0Jxy0e9KngBaTDwG_ZuSE_TVfjn_GmA5FoSeuaHrD9KN6vRNYBtmaCbAYPK8N90tF0BBmR-24jt-8xm5QC53fpepnWQiGXCtt"
-        kkyu = "doQLVh518o8:APA91bFl027SHigp2b1aarCQUfCUaQ-RGOrdU3LYOne-JJlga34bnaeVOnFyW3P9GKwKFqMIFTuHdkKQh4_fGn58DWq9RAlonyAUyFznxw61wgbvnhPZiaCpxoQ3j76EIWMCC_tS037I"
+        #sup = "fOqbayB9yCq3o:APA91bHH4fQu5Bipea_9QEYsnduX0kQ_bMa7FlcaMeP0Jxy0e9KngBaTDwG_ZuSE_TVfjn_GmA5FoSeuaHrD9KN6vRNYBtmaCbAYPK8N90tF0BBmR-24jt-8xm5QC53fpepnWQiGXCtt"
+#        kkyu = "doQLVh518o8:APA91bFl027SHigp2b1aarCQUfCUaQ-RGOrdU3LYOne-JJlga34bnaeVOnFyW3P9GKwKFqMIFTuHdkKQh4_fGn58DWq9RAlonyAUyFznxw61wgbvnhPZiaCpxoQ3j76EIWMCC_tS037I"
         wohn = "dYQR2hgIZ50:APA91bE3TSTqsTIwnNmGhNFfQJQfiW7tcKWAozmZuGeXGBBxMdhZc5LTYL4NzsJbOLug_z-8Ia0ts-IQhdsllZmwFwLTETTnHqUeh8otq7Edbz2Wwp-Pu6qZo9Wq3cSdZunfhUmQB4-S"
-        yongand = "dMOBkHhz344:APA91bEVoAaHpDJGQ5zIvwpPD_xcKe8Ncl8mP3387h9aKZLLoo8NbJik7lz7Sg6w4lFsUOYwyNxdrKrLkcfXRicoS10JFIhhfPcOHpkjky148SI-yiUCe9M7k9zEn7W-pZtGeL79UM9r"
-        sup = "fOqyB9yCq3o:APA91bHH4fQu5Bipea_9QEYsnduX0kQ_bMa7FlcaMeP0Jxy0e9KngBaTDwG_ZuSE_TVfjn_GmA5FoSeuaHrD9KN6vRNYBtmaCbAYPK8N90tF0BBmRjn_GmA5FoSeuaHrD9KN6vRNYBtmaCbAYPK8N90tF0BBmR-24jt-8xm5QC53fpepnWQiGXCtt"
+#        yongand = "dMOBkHhz344:APA91bEVoAaHpDJGQ5zIvwpPD_xcKe8Ncl8mP3387h9aKZLLoo8NbJik7lz7Sg6w4lFsUOYwyNxdrKrLkcfXRicoS10JFIhhfPcOHpkjky148SI-yiUCe9M7k9zEn7W-pZtGeL79UM9r"
+#        sup = "fOqyB9yCq3o:APA91bHH4fQu5Bipea_9QEYsnduX0kQ_bMa7FlcaMeP0Jxy0e9KngBaTDwG_ZuSE_TVfjn_GmA5FoSeuaHrD9KN6vRNYBtmaCbAYPK8N90tF0BBmRjn_GmA5FoSeuaHrD9KN6vRNYBtmaCbAYPK8N90tF0BBmR-24jt-8xm5QC53fpepnWQiGXCtt"
 
         #listname = [kkyu,wohn]
         #for i in listname:
 
         tokens.append(wohn)
-        tokens.append(kkyu)
-        tokens.append(yongand)
-        tokens.append(sup)
+ #       tokens.append(kkyu)
+ #       tokens.append(yongand)
+ #       tokens.append(sup)
         try:
             result = push_service.notify_multiple_devices(registration_ids=tokens, message_title=title, message_body=message,
-                                                                                              data_message={})
+                                                                                               data_message={})
         except Exception as e:
-            print e
-            print str(result)
+            print (e)
+            print (str(result))
+        data = {
+                "result" : 0,
+                "message":"success",
+                "crawler_id":[
+                    
+                    ]
+                }
+#       body = urllib.parse.urlencode(data)
+#        h = httplib2.Http()
+#       resp, content = h.request("http://52.78.113.6:8000/subscriber_pushtoken/", method="POST", body=body)
     
-    
-    
-    
+        data1 = request.POST['title']
+        data2 = request.POST['token']
+        data3 = request.POST['body']
+
+        print(data1)
+        print(data2)
+        print(data3)
+
 #    currentDir = subprocess.check_output(["pwd"])
 #    currentDir = currentDir[:-1]
 #    subprocess.call(["python "+currentDir+"/push/pushing.py"], shell=True)
