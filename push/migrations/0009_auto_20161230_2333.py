af6b540e3f61eb3d4362cdefef82fb5271412366
Wohn Joonho <doomsheart@naver.com>
2017-01-02 12:55:01 +0900

    pushing error resolved

---
+++ push/migrations/0009_auto_20161230_2333.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('push', '0008_auto_20161228_2346'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='crawldata',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2016, 12, 30, 23, 33, 40, 598234)),
+        ),
+    ]
