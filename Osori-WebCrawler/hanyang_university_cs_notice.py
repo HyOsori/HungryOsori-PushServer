78bd2974214e0aefecc325b8880deacd3dc9bff6
epikjjh <epikjjh@gmail.com>
2017-01-04 16:36:08 +0900

    cs_notice_changed

--- Osori-WebCrawler/hanyang_university_cs_notice.py
+++ Osori-WebCrawler/hanyang_university_cs_notice.py
@@ -1,23 +1,24 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 import logging
+import datetime
 
 class CSHanyangCrawler(scrapy.Spider):
     name = 'cs.hanyang - crawler'
-    start_urls = ['http://cs.hanyang.ac.kr/?page=intro/intro03']
-    base_urls = "http://cs.hanyang.ac.kr/?page=intro/intro03&mode=read&msg="
+    start_urls = ['http://cs.hanyang.ac.kr/board/info_board.php']
+    base_urls = "http://cs.hanyang.ac.kr/board/info_board.php?ptype=view&idx="
     logging.getLogger('scrapy').propagate = False
 
     def parse(self, response):
-        query_notice_title = '.tover > div > a::text'
-        query_notice_number = 'body > table > tr:nth-child(2) > td > table > tr:nth-child(2) > td:nth-child(2) >' \
-                              ' div > table:nth-child(7) > tr > td > table > tr:nth-child(1) > td:nth-child(1) >' \
-                              ' font::text'
+        query_notice_urls = "#content_in > div > table > tbody > tr > td > a::attr(href)"
+        query_notice_number = "#content_in > div > table > tbody > tr > td"
+
+        default_time = int(datetime.datetime.now().timestamp()) * 10
 
         numbers = [int(text) for text in response.css(query_notice_number).extract() if text.isdigit()]
-        titles = response.css(query_notice_title).extract()
+        urls = response.css(query_notice_urls).extract()
 
-        pairs = zip(numbers, titles)
+        pairs = zip(numbers, urls)
 
         for number, pair in pairs:
 #            print (number, pair)
