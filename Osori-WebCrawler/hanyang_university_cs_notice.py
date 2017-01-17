0a59ca925af29d01f8d236c4ffd21b8ab2b2fa7f
epikjjh <epikjjh@gmail.com>
2017-01-17 21:34:18 +0900

    cs_notice revised

--- Osori-WebCrawler/hanyang_university_cs_notice.py
+++ Osori-WebCrawler/hanyang_university_cs_notice.py
@@ -4,31 +4,25 @@ import logging
 import datetime
 
 class CSHanyangCrawler(scrapy.Spider):
-    name = 'cs.hanyang - crawler'
-    start_urls = ['http://cs.hanyang.ac.kr/board/info_board.php']
-    base_urls = "http://cs.hanyang.ac.kr/board/info_board.php?ptype=view&idx="
+    name = "cs.hanyang - crawler"
+    allowed_domains = ["cs.hanyang.ac.kr"]
+    start_urls = ["http://cs.hanyang.ac.kr/board/info_board.php"]
+    base_urls = "http://cs.hanyang.ac.kr"
     logging.getLogger('scrapy').propagate = False
 
     def parse(self, response):
-<<<<<<< HEAD
-        query_notice_urls = "#content_in > div > table > tr:nth-child(25) > td.left > a::attr(href)"
-        query_notice_number = "#content_in > div > table > tr:nth-child(25) > td:nth-child(2)"
-=======
-      
-        query_notice_urls = "#content_in > div > table > tbody > tr > td > a::attr(href)"
-        query_notice_number = "#content_in > div > table > tbody > tr > td"
+        query_notice_urls = "#content_in > div > table > tr > td.left > a::attr(href)"
+        query_notice_title = "#content_in > div > table > tr > td.left > a::text"
 
->>>>>>> 9b430f7ed3a87e2c6788fc29761559500cdf07d9
         default_time = int(datetime.datetime.now().timestamp()) * 10
 
-        numbers = [int(text) for text in response.css(query_notice_number).extract() if text.isdigit()]
         urls = response.css(query_notice_urls).extract()
+        texts = response.css(query_notice_title).extract()
 
+        pairs = zip(texts,urls)
 
-        pairs = zip(numbers, urls)
-        for number, pair in pairs:
-#            print (number, pair)
-            print(str(number) + "&^%987&^%" + pair + "&^%987&^%" + self.base_urls +str(number)+"&subkind=&offset=0")
+        for title,url in pairs:
+            print(title + "&^%987&^%" + self.base_urls + url + "&subkind=&offset=0")
 
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
