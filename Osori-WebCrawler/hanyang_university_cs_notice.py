0ea948b14bab707107daca7cded1f5f4f5187a19
epikjjh <epikjjh@gmail.com>
2017-10-19 21:07:00 +0900

    Crawler revised - Hanynag cs notice

--- Osori-WebCrawler/hanyang_university_cs_notice.py
+++ Osori-WebCrawler/hanyang_university_cs_notice.py
@@ -1,32 +1,16 @@
-import scrapy
-from scrapy.crawler import CrawlerProcess
-import logging
-import datetime
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-class CSHanyangCrawler(scrapy.Spider):
-    name = "cs.hanyang - crawler"
-    allowed_domains = ["cs.hanyang.ac.kr"]
-    start_urls = ["http://cs.hanyang.ac.kr/board/info_board.php"]
-    base_urls = "http://cs.hanyang.ac.kr"
-    logging.getLogger('scrapy').propagate = False
+html = urlopen("http://cs.hanyang.ac.kr/board/info_board.php")
+source = "http://cs.hanyang.ac.kr/board/info_board.php"
+soup = BeautifulSoup(html, "html.parser")
+results = soup.find_all('td', {'class' : 'left'})
 
-    def parse(self, response):
-        query_notice_urls = "#content_in > div > table > tr > td.left > a::attr(href)"
-        query_notice_title = "#content_in > div > table > tr > td.left > a::text"
+for result in results:
+    text = result.find('a').get_text()
+    link = result.find('a')['href']
 
-        default_time = int(datetime.datetime.now().timestamp()) * 10
-
-        urls = response.css(query_notice_urls).extract()
-        texts = response.css(query_notice_title).extract()
-
-        pairs = zip(texts,urls)
-
-        for title,url in pairs:
-            print(title + "&^%987&^%" + self.base_urls + url + "&subkind=&offset=0")
-
-process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-})
-
-process.crawl(CSHanyangCrawler)
-process.start()
+    if link.find("http") == -1:
+        print("Notice : " + text + " / Link : "+ source + link)
+    else:
+        print("Notice : " + text + " / Link : "+ link)
