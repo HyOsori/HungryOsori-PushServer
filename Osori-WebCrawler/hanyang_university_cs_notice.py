b722d1367d8677ff08a34c6dd46ac44f18ff2e78
doomsheart@naver.com <doomsheart@naver.com>
2018-02-28 11:34:21 +0900

    add bs4 crawlers

--- Osori-WebCrawler/hanyang_university_cs_notice.py
+++ Osori-WebCrawler/hanyang_university_cs_notice.py
@@ -1,16 +1,24 @@
+# -*- coding:utf-8 -*-
+# -*- coding: euc-kr -*-
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen("http://cs.hanyang.ac.kr/board/info_board.php")
 source = "http://cs.hanyang.ac.kr/board/info_board.php"
 soup = BeautifulSoup(html, "html.parser")
-results = soup.find_all('td', {'class' : 'left'})
+results = soup.find_all('td', {'class': 'left'})
 
 for result in results:
     text = result.find('a').get_text()
     link = result.find('a')['href']
 
     if link.find("http") == -1:
-        print("Notice : " + text + " / Link : "+ source + link)
+        try:
+            print(("Notice : " + text + " / Link : " + source + link))
+        except:
+            print(("Notice : " + link + " / Link : " + link))
     else:
-        print("Notice : " + text + " / Link : "+ link)
+        try:
+            print(("Notice : " + text + " / Link : " + link))
+        except:
+            print("error")
